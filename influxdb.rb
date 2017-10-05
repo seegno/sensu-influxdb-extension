d489c189d7d5f7d4b2d52767f9e09a51f95e8d1e
naemono <mmontg1@gmail.com>
2017-10-05 15:07:18 -0500

    Use sensu client tags

--- influxdb.rb
+++ influxdb.rb
@@ -64,7 +64,7 @@ module Sensu::Extension
         key.gsub!("\\"){ "\\\\" }
 
         # This will merge : default conf tags < check embedded tags < sensu client/host tag
-        tags = @influx_conf['tags'].merge(event[:check][:influxdb][:tags]).merge({'host' => client})
+        tags = @influx_conf['tags'].merge(event[:check][:influxdb][:tags]).merge(event[:client][:tags]).merge({'host' => client})
         tags.each do |tag, val|
           key += ",#{tag}=#{val}"
         end
